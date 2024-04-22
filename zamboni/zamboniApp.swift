71f0c796d3f86204d9ea33a2608059f94870996d
Stephen Birarda <commit@birarda.com>
2024-04-22 23:17:32 +0200

    Initial Commit

---
+++ zamboni/zamboniApp.swift
@@ -0,0 +1,17 @@
+//
+//  zamboniApp.swift
+//  zamboni
+//
+//  Created by Stephen on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct zamboniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
